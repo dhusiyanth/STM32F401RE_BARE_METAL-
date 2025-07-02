b4b805636c8a1bcdde418a4954ce126958620220
M.DHUSIYANTH <94106634+dhusiyanth@users.noreply.github.com>
2025-07-02 10:45:54 +0530

    Update main.c

--- Led_Blink/main.c
+++ Led_Blink/main.c
@@ -1,1 +1,27 @@
+/*
+ * File Name    : Led_Blink.c
+ * Author       : Dhusiyanth M
+ * Description  : Blinks an LED connected to PA5 using STM32F401RE in bare-metal (CMSIS) style
+ * Date         : 16-06-2025
+ */
+#include"stm32f4xx.h"
+void delay(int time)
+{
+	while(time--);
+}
+int main()
+{
+	RCC->AHB1ENR |=(1<<0);
+	GPIOA->MODER &=~(3<<10);
+	GPIOA->MODER |=(1<<10);
+  
+	
+	while(1)
+	{
+		GPIOA->ODR ^=(1<<5);
+		delay(1000000);
+	}
+}
+
+
 
